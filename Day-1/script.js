49fbb0b91d85b5f44be0cee4c98882f9a4946e93
aditya <shawaditya009@gmail.com>
2026-01-29 15:43:18 +0530

    revice old concept

--- Day-1/script.js
+++ Day-1/script.js
@@ -1,7 +1,36 @@
-console.log("This program add three numbers");
+// console.log("This program add three numbers");
 
-function addThreeNumbers(a, b, c){
-    return a+b+c; 
-}
-let c=addThreeNumbers(1,2,54);
-console.log(c);
+// function addThreeNumbers(a, b, c){
+//     return a+b+c; 
+// }
+// let c=addThreeNumbers(1,2,54);
+// console.log(c);
+// ## Variables (let, var, const)
+// let a = 67;
+
+// console.log(a);
+
+// a = "aditya";
+
+// console.log(a);
+
+// ## VAR is a global scoped variable and can be declared many times. Not used in ECMAscript 6.
+
+// var a = 45;
+// console.log(a);
+// var a="p";
+// console.log(a);
+
+//## LET is a block scoped variable.
+
+// let b="STring";
+// console.log(b);
+// b=96;
+// console.log(b);
+// let c=null;
+// { 
+//     let c = "Not null";
+//     console.log(c);
+//  }
+// console.log(c);
+
