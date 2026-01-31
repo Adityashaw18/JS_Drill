3238ff6cd633b5603a6007bbc8816ff874744966
aditya <shawaditya009@gmail.com>
2026-01-31 17:38:48 +0530

    constructor

--- Day-2/index.js
+++ Day-2/index.js
@@ -2,12 +2,32 @@ console.log('Chaliye suru krte hai');
 
 //object create
 
-const rectangle = {
-    length: 1,
-    breadth: 2,
+// const rectangle = {
+//     length: 1,
+//     breadth: 2,
 
-    draw: function() {
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
+
+// function createRectangle(length, breadth) {
+//     return rectangle = {
+//         length,
+//         breadth,
+
+//         draw: function() {
+//             console.log('drawing rectangle');
+//         }
+//     }
+// }
+
+// let rectangleobj1 = createRectangle(5, 4);
+
+function Rectangle() {
+    this.length = 1;
+    this.breadth = 2;
+    this.draw = function() {
         console.log('drawing rectangle');
     }
-};
-// console.log(rerctangle);
+}
