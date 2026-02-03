71edc819598af4d5f3feee82be364fa99980a71f
aditya <shawaditya009@gmail.com>
2026-02-04 02:42:41 +0530

    update day-2

--- Day-2/index.js
+++ Day-2/index.js
@@ -33,46 +33,69 @@ console.log('Chaliye suru krte hai');
 
 //factory function
 
-function createRectangle(length,breadth){
-    return rectangle = {
-    length,
-    breadth,
+// function createRectangle(length,breadth){
+//     return rectangle = {
+//     length,
+//     breadth,
 
-    draw: function() {
-        console.log('drawing rectangle');
-    }
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
+// }
+
+// let rectangleobj1  = createRectangle(5,2);
+// let rectangle2 = createRectangle(9,6);
+// let rectangle3 = createRectangle(8,3);
+
+// //Constructor function -> pascal Notation
+
+// function Rectangle() {
+//     this.length = 1;
+//     this.breadth =2;
+//     this.draw = function(){
+//         console.log('Art_Making');
+//     }
+// }
+
+// //object creation using constructor function
+// let rectangleObject = new Rectangle();
+
+// rectangleObject.color = 'yellow';
+// console.log(rectangleObject);
+
+// delete rectangleObject.color;
+// console.log(rectangleObject);
+
+// let Rect = new Function(
+//     'length','breadth',
+//     `this.length = length;
+//     this.breadth =breadth;
+//     this.draw = function(){
+//         console.log('Art_Making');
+//     }`
+// );
+
+// let a ={value: 10};
+// let b = a;
+
+// a.value++;
+
+// console.log(a.value);
+// console.log(b.value);
+
+// let r =10;
+// function inc(r){
+//     r++;
+// }
+// inc(r);
+// console.log(r);
+
+let rectangle = {
+    length : 2,
+    breadth : 4
 };
-}
-
-let rectangleobj1  = createRectangle(5,2);
-let rectangle2 = createRectangle(9,6);
-let rectangle3 = createRectangle(8,3);
-
-//Constructor function -> pascal Notation
-
-function Rectangle() {
-    this.length = 1;
-    this.breadth =2;
-    this.draw = function(){
-        console.log('Art_Making');
-    }
-}
-
-//object creation using constructor function
-let rectangleObject = new Rectangle();
-
-rectangleObject.color = 'yellow';
-console.log(rectangleObject);
-
-delete rectangleObject.color;
-console.log(rectangleObject);
-
-let Rect = new Function(
-    'length','breadth',
-    `this.length = length;
-    this.breadth =breadth;
-    this.draw = function(){
-        console.log('Art_Making');
-    }`
-);
-let k=new Rect(5,7);
+//for-of loop
+// for(let key in rectangle){
+//     console.log(key.rectangle(key));
+// }
