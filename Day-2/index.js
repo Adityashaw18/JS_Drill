515f994b5a4d906ddb9585bd0e549deae79ee1b9
aditya <shawaditya009@gmail.com>
2026-02-04 01:45:47 +0530

    revising

--- Day-2/index.js
+++ Day-2/index.js
@@ -2,7 +2,7 @@ console.log('Chaliye suru krte hai');
 
 //object create
 
-// const rectangle = {
+// let rectangle = {
 //     length: 1,
 //     breadth: 2,
 
@@ -11,23 +11,68 @@ console.log('Chaliye suru krte hai');
 //     }
 // };
 
-// function createRectangle(length, breadth) {
-//     return rectangle = {
-//         length,
-//         breadth,
+//object oriented programming
 
-//         draw: function() {
-//             console.log('drawing rectangle');
-//         }
+// const rectangle1 = {
+//     length: 1,
+//     breadth: 2,
+
+//     draw: function() {
+//         console.log('drawing rectangle');
 //     }
-// }
+// };
 
-// let rectangleobj1 = createRectangle(5, 4);
+// const rectangle2 = {
+//     length: 1,
+//     breadth: 2,
+
+//     draw: function() {
+//         console.log('drawing rectangle'); 
+//     }
+// };
+
+//factory function
+
+function createRectangle(length,breadth){
+    return rectangle = {
+    length,
+    breadth,
+
+    draw: function() {
+        console.log('drawing rectangle');
+    }
+};
+}
+
+let rectangleobj1  = createRectangle(5,2);
+let rectangle2 = createRectangle(9,6);
+let rectangle3 = createRectangle(8,3);
+
+//Constructor function -> pascal Notation
 
 function Rectangle() {
     this.length = 1;
-    this.breadth = 2;
-    this.draw = function() {
-        console.log('drawing rectangle');
+    this.breadth =2;
+    this.draw = function(){
+        console.log('Art_Making');
     }
 }
+
+//object creation using constructor function
+let rectangleObject = new Rectangle();
+
+rectangleObject.color = 'yellow';
+console.log(rectangleObject);
+
+delete rectangleObject.color;
+console.log(rectangleObject);
+
+let Rect = new Function(
+    'length','breadth',
+    `this.length = length;
+    this.breadth =breadth;
+    this.draw = function(){
+        console.log('Art_Making');
+    }`
+);
+let k=new Rect(5,7);
