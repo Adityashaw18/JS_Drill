5b9a4bfc257dd6d71c6daadc411f187e776d8d83
aditya <shawaditya009@gmail.com>
2026-02-04 16:45:25 +0530

    new js concepts

---
+++ Day-3/index.js
@@ -0,0 +1,27 @@
+console.log('Rom Rom Bhaiyo!');
+//primitive datatype
+let lastname = 'Shaw';
+//object
+let firstname = new String("Aditya");
+
+let message = 
+`Hello ${firstname},
+
+Thanks for the Opportunity
+
+Regards,
+Shaw`;
+
+// let words = message.split(" ");
+console.log(message);
+
+///Date And time
+
+let date = new Date();
+
+let date1 = new Date("March 18 2002 6:45");
+
+let date2 = new Date(2002, 2, 18, 6);
+
+date2.setFullYear(1947)
+console.log(date2);
