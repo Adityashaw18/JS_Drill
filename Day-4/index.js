1d4f2b4b6187a183d15e1ffd5ab523538859566f
aditya <shawaditya009@gmail.com>
2026-02-06 00:19:41 +0530

    breaking functions and learning how it works

--- Day-4/index.js
+++ Day-4/index.js
@@ -1,25 +1,94 @@
 console.log("Kya Haal Bhai ke!");
 
-//function Declaration
-function run(){
-	console.log("running");
-}
+// //function Declaration
+// function run(){
+// 	console.log("running");
+// }
 
-//function call or invoke
-run();
+// //function call or invoke
+// run();
 
-//Named function assignment
-let stand = function walk() {
-	console.log('walking');
-};
+// //Named function assignment
+// let stand = function walk() {
+// 	console.log('walking');
+// };
 
-let jump=stand;
+// let jump=stand;
 
-jump();
+// jump();
 
-//Anonymousfunction assignment
-let stand2 = function () {
-	console.log('walking+running');
-}
+// //Anonymousfunction assignment
+// let stand2 = function () {
+// 	console.log('walking+running');
+// }
 
-stand2();
+// stand2();
+
+// function sum(a,b) {
+// 	return a+b;
+// }
+
+
+// function sum(a,b) {
+// 	console.log(arguments);
+// 	return a+b;
+// }
+
+
+// function sum() {
+// 	 total = 0;
+// 	for(let value of arguments){
+// 		total = total + value;
+// 	}
+// 	return total;
+// };
+
+// let ans=sum(1,2,3,4,5,6,7,8,9);
+// console.log(ans);
+
+// //Rest parameters
+// function sum(...args){
+// 	console.log(args);
+// }
+// sum(1,2,3,4,5,6,7,8,9);
+
+//Default parameters
+// function intrest(p,r=6,y=9){
+// 	return p*r*y/100;
+// }
+
+
+// console.log(intrest(1000,undefined,8));
+
+
+// let person = { 
+// 	fName : 'Aditya',
+// 	lName : 'Shaw'
+// };
+
+// function fullname() {
+// 	return `${person.fName} ${person.lName}`;
+// }
+
+// console.log(fullname());
+
+//GETTER SETTER
+
+// let person = { 
+// 	fName : 'Aditya',
+// 	lName : 'Shaw',
+// 	get fullname() {
+// 	return `${person.fName} ${person.lName}`;
+// 	},
+// 	set fullname(value) {
+// 		let parts = value.split(' ');
+// 		this.fName = parts[0];
+// 		this.lName = parts[1];
+		
+// 	}
+// };
+
+// // console.log(person.fullname);
+
+// person.fullname = 'Rahul kumar';
+// console.log(person.fullname);
